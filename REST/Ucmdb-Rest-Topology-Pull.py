06c687fe5da70360f687b9c572a77b0d8c308f52
jperry-intact <38509302+jperry-intact@users.noreply.github.com>
2018-09-19 10:47:51 -0700

    Added variables

--- REST/Ucmdb-Rest-Topology-Pull.py
+++ REST/Ucmdb-Rest-Topology-Pull.py
@@ -2,14 +2,23 @@ import string
 import requests
 import ast 
 
-task = {"username":"****UCMDB_USER****","password":"****UCMDB_PASSWORD****","clientContext":1}
-authresp = requests.post('https://****UCMDB_HOST****:****UCMDB_PORT****/rest-api/authenticate', json=task, verify=False)
+ucmdb_user = "****UCMDB_USER****"
+ucmdb_password = "****UCMDB_PASSWORD****"
+ucmdb_root_url_and_port = "****UCMDB_PROTOCOL****://****UCMDB_HOST****:****UCMDB_PORT****"
+ucmdb_tql_query = "****UCMDB_TQL_QUERY****"
+
+ucmdb_auth_uri = ucmdb_root_url_and_port + "/rest-api/authenticate"
+ucmdb_topology_uri = ucmdb_root_url_and_port + "/rest-api/topology"
+
+task = {"username":ucmdb_user,"password":ucmdb_password,"clientContext":1}
+authresp = requests.post(ucmdb_auth_uri, json=task, verify=False)
+
 authjsonReturn = authresp.json()
 authParsedValue = authjsonReturn['token']
 auth_value = "'Authorization': 'Bearer " + authParsedValue +"'"
 auth_cookie = "{" + auth_value + "}"
 dict_auth = ast.literal_eval(auth_cookie)
-tql_query = "****UCMDB_TQL_QUERY****"
-topologyresp = requests.post('https://****UCMDB_HOST****:****UCMDB_PORT****/rest-api/topology', headers=dict_auth, data=tql_query, verify=False)
+
+topologyresp = requests.post(ucmdb_topology_uri, headers=dict_auth, data=ucmdb_tql_query, verify=False)
 topologyjsonReturn = topologyresp.json()
 print(topologyjsonReturn)
